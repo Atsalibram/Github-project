1aa774a4978ae2f2abf29d61fd306217adc9cb84
atsalibram <atsalibaram@gmail.com>
2021-02-10 14:22:26 +0300

    added h2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,10 @@
 
 export const environment = {
   production: false,
-
+  apiUrl:"https://api.github.com/users/",
+  apiKey:"9cde2ba42d9a2626581098b290da33c203205aa5"
+  
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+
